9123d7df5778b6d671b7fbecd99f8ef4ee53fe7d
Aniket Raut <rautan_1@rknec.edu>
2024-06-27 01:47:11 +0530

    Made Home Page

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,12 +1,42 @@
 import "./Navbar.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import {
+  CircleUserRound,
+  House,
+  TvMinimalPlay,
+  SquareArrowLeft,
+} from "lucide-react";
+import { useTma } from "../../context/tmaProvider";
 
 function Navbar() {
+  const { user } = useTma();
+
+  const size = 30;
   return (
-    <div>
-      Navbar
+    <>
+      <nav className="upperNavbar">
+        <Link>
+          <CircleUserRound size={38} />
+          {
+            <p>
+              {user?.firstName} {user?.lastName}
+            </p>
+          }
+        </Link>
+      </nav>
       <Outlet />
-    </div>
+      <nav className="lowerNavbar">
+        <Link>
+          <House size={size} />
+        </Link>
+        <Link>
+          <TvMinimalPlay size={size} />
+        </Link>
+        <Link>
+          <SquareArrowLeft size={size} />
+        </Link>
+      </nav>
+    </>
   );
 }
 
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,7 +1,72 @@
+import { Link } from "react-router-dom";
 import "./Home.css";
+import { CircleHelp, Clock, RefreshCcw } from "lucide-react";
 
 function Home() {
-  return <div>Home</div>;
+  const domainList = [
+    {
+      name: "Geography",
+      ref: "#",
+      img: "/geo.png",
+    },
+    {
+      name: "Technology",
+      ref: "#",
+      img: "/tech.png",
+    },
+    {
+      name: "General Knowledge",
+      ref: "#",
+      img: "/gk.png",
+    },
+  ];
+  const Knowledge = [
+    {
+      _id: "667b17a22b49d9acf21ed9ad",
+      fact: "A bolt of lightning contains enough energy to toast 100,000 slices of bread.",
+      category: "Weather",
+    },
+  ];
+  return (
+    <div className="Home">
+      <img src="/landing.png" className="landingImg" alt="" />
+      <h2>Domains</h2>
+      <div className="domainsContainer">
+        {domainList.map((domain, index) => (
+          <Link to={domain.ref} key={index}>
+            <img src={domain.img} alt="" />
+            <p>{domain.name}</p>
+          </Link>
+        ))}
+      </div>
+      <div className="fotdCard">
+        <span>
+          <Clock />
+          Fact of the day !
+        </span>
+        <p>{Knowledge[0].fact}</p>
+      </div>
+      <div className="fotdCard">
+        <span>
+          <CircleHelp />
+          Do You Know that?
+          <RefreshCcw
+            style={{
+              marginLeft: "auto",
+            }}
+          />
+        </span>
+        <p>{Knowledge[0].fact}</p>
+      </div>
+      <div className="ninjamsg">
+        <img src="/landingninja.png" alt="" />
+        <p>
+          Hello! I am Mimo , your Ninja-Assistant ! Check out the middle TV-icon
+          for our Knowledge Cubes.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
